9c092aa67a22f035c81f7502db9703e362151934
naomiacc <fanny.ayello.campos@outlook.com>
2022-09-06 18:01:22 +0200

    feat : comments

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,7 +3,7 @@ const uniqueValidator = require("mongoose-unique-validator");
 
 const userModels = mongoose.Schema({
   email: { type: "string", require: "true", unique: true }, // unique true : il est impossible de s'inscrire plusieurs fois avec la même adresse mail
-  password: { type: "string", require: "true" },
+  password: { type: "string", require: "true" }, // mot de passe de l'utilisateur haché
 });
 
 userModels.plugin(uniqueValidator);
