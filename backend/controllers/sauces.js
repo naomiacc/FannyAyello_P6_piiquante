e4b906ac39b332b76576a0d99ba480537548e384
naomiacc <fanny.ayello.campos@outlook.com>
2022-09-13 20:16:42 +0200

    fix : error createSauce

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -17,21 +17,22 @@ exports.getOneSauce = (req, res, next) => {
 // Creation d'une sauce
 exports.createSauce = (req, res, next) => {
   const sauceObject = JSON.parse(req.body.sauce);
+  console.log(sauceObject);
+  console.log(req.file.filename);
+
   delete sauceObject._id;
   const sauce = new Sauce({
     ...sauceObject,
     imageUrl: `${req.protocol}://${req.get("host")}/images/${
       req.file.filename
     }`,
-    likes: 0,
-    dislikes: 0,
-    usersLiked: ["string"],
-    usersDisliked: ["string"],
   });
+  console.log(sauce);
+
   sauce
     .save()
-    .then(() => res.status(201).json({ message: "Sauce enregistrée !" }))
-    .catch((error) => res.status(400).json({ error }));
+    .then(() => res.status(201).json({ message: "La sauces à été crée" }))
+    .catch((err) => res.status(400).json(err));
 };
 
 //modification d'une sauce
